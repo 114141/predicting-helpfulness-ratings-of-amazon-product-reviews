daee1b085bbe041b4f96117dc656a94f039c7d5c
Anna <12577888+114141@users.noreply.github.com>
2018-11-06 07:15:50 -0500

    Update processing_amazonproductreviewprediction.R

--- processing_amazonproductreviewprediction.R
+++ processing_amazonproductreviewprediction.R
@@ -98,6 +98,91 @@ subsetforcor <- ten_newdf[c(1,2,4,5)]
 corrplot(cor(subsetforcor), method="color")
 #There is a small correlation between the 'helpful' rating and our score.
 
+#plotting histograms
+overall <- ten_newdf$overall
+hist(overall)
+
+#cleaning text as a part of preprocess (using tm package)
+reviews <- ten_newdf$reviewText
+
+review_corpus <- Corpus(VectorSource(reviews))
+
+#change to lowercase
+review_corpus <- tm_map(review_corpus, content_transformer(tolower))
+#remove numbers
+review_corpus <- tm_map(review_corpus, removeNumbers)
+#remove punctuation
+review_corpus <- tm_map(review_corpus, removePunctuation)
+#remove stop words and "the" and "and"
+review_corpus <- tm_map(review_corpus, removeWords, c("the", "and", stopwords("english")))
+#stemming words
+review_corpus <- tm_map(review_corpus, stemDocument, language = "english")
+#removing white space
+review_corpus <- tm_map(review_corpus, stripWhitespace)
+
+#checking out corpus final outcome
+inspect(review_corpus[1])
+
+#creating a document-term matrix: where documents 
+#are rows, and the words are the columns. 
+#frequency of the term in the document are the entries.
+review_doctm <- DocumentTermMatrix(review_corpus)
+review_doctm
+
+
+#inspecting five rows of matrix
+inspect(review_doctm[305:310, 305:310])
+
+
+
+#the sparsity is very high. we will remove low frequency words
+#as they are possibly typos or otherwise uninteresting
+review_doctm = removeSparseTerms(review_doctm, 0.98)
+review_doctm
+
+
+#inspecting five rows of matrix again
+inspect(review_doctm[305:310, 305:310])
+
+
+#finding tf-idf which finds the relative importance of a word to a document.
+reviewdoctm_tfidf <- DocumentTermMatrix(review_corpus, control = list(weighting = weightTfIdf))
+#this process is noting that there are empty reviews (ie. document) even though they did not come up as empty in preprocess
+#need to recheck
+#now creating tf-idf scores
+reviewdoctm_tfidf = removeSparseTerms(reviewdoctm_tfidf, 0.94)
+reviewdoctm_tfidf
+
+#checking the first document
+inspect(reviewdoctm_tfidf[1,1:15])
+
+#creating new dataframe for reviews and tf-idf to inspect before adding as feature to full dataset
+n_reviews <- cbind(reviews, as.matrix(reviewdoctm_tfidf))
+#view sample of new datafram
+head(n_reviews)
+
+
+#adding the tfidf column to the working dataset
+ten_newdf$tfidf <- as.matrix(reviewdoctm_tfidf)
+
+#checking class of new column
+sapply(ten_newdf, class)
+#checking first few rows of new tfidf column
+head(ten_newdf$tfidf)
+
+
+#spliting train and testing sets
+trainrows <- sample(nrow(ten_newdf),nrow(ten_newdf)*0.80)
+ten_newdf.train = ten_newdf[trainrows,]
+ten_newdf.test = ten_newdf[-trainrows,]
+
+#logistic regression model
+ten_newdf.glm = glm(helpful~ ., family = "binomial", data=ten_newdf.train, maxit = 100); 
+
+#evaluate logistic regression model
+predict_glm = as.numeric(predict(ten_newdf.glm, ten_newdf.test, type="response") > 0.5)
+table(ten_newdf.test$helpful,predict_glm,dnn=c("Observed","Predicted"))
+
 
 
 
